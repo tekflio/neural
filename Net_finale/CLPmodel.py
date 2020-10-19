7fa48eb8a5f05fc9e27b1fa46ed0cea019ccc137
tekflio <62204359+tekflio@users.noreply.github.com>
2020-10-19 15:56:05 +0200

    Add files via upload

--- Net_finale/CLPmodel.py
+++ Net_finale/CLPmodel.py
@@ -10,12 +10,19 @@ import fastmri
 from fastmri.data import transforms as T
 class Net(nn.Module):
     def __init__(self, ksize=5, num=2):
+        #Here we define the Neural Network Structure
         super(Net, self).__init__()
         self.shuffle_down_4 = ComplexShuffleDown(4)
         self.shuffle_up_4 = ComplexShuffleUp(4)
         self.shuffle_up_2 = ComplexShuffleUp(2)
-        self.backbone = Backbone(16, 64)
+        self.convBlock1 = ConvBlock1(16, 64)
 
+        # After the 4x Downsampling we have to split the computation in 3 branches
+        # Each will execute a different operation that will result in combining the output images with the
+        # laplacian decomposition results
+
+        # Each of the followings is the ConvBlock2, we needed to change the input/output channels
+        # in order to align to the paper specifics
         self.branch1 = nn.Sequential(ComplexConv2d(4, 64, 3, 1, 1),
                                  nn.ReLU(),
                                  ComplexConv2d(64, 64, 3, 1, 1),
@@ -34,30 +41,44 @@ class Net(nn.Module):
                                  nn.ReLU(),
                                  ComplexConv2d(64, ksize ** 2, 3, 1, 1),
                                  nn.ReLU())
+
+        #Per-pixel convolution operation
         self.pixel_conv = PerPixelConv()
         self.pyramid_decompose = PyramidDecompose()
         self.pyramid_reconstruct = PyramidReconstruct()
+        #num is the cascade number
         self.num = num
 
     def forward(self, temp, x_k, mask):
+        # Loop over num = cascades number
         for i in range(self.num):
-            #print("Cascade n° (",i+1,")")
+
+            #The laplacian decomposition will produce different images composing the gaussian/laplacian pyramids
+            # The gaussians are the downsampled images from the original one
+            # The laplacian represents the 'errors' within the images related to the gaussian images
             gaussian_pyramid, laplacian_pyramid = self.pyramid_decompose(temp)
+
+            # We need the first two bigger images
             lap_1, lap_2 = laplacian_pyramid[0], laplacian_pyramid[1]
+
+            #We need the smallest gaussian image
             gaussian_3 = gaussian_pyramid[-1]
-            # x.size() = torch.Size([Batch, 1, 256, 256])
-            temp = self.shuffle_down_4(temp)          # torch.Size([Batch, 16, 64, 64])
-            temp = self.backbone(temp)                # torch.Size([Batch, 64, 64, 64])
-            
+
+            #Resize along with the input/output channels
+            temp = self.shuffle_down_4(temp)
+
+            #The convBlock1 is here performed to extract shallow features
+            temp = self.convBlock1(temp)
+            # ---- 3 branches ----
             # 4x
-            branch_1 = self.shuffle_up_4(temp)     # torch.Size([Batch, 4, 256, 256])
-            branch_1 = self.branch1(branch_1)   # torch.Size([Batch, 25, 256, 256])
+            branch_1 = self.shuffle_up_4(temp)
+            branch_1 = self.branch1(branch_1)
             # 2x
-            branch_2 = self.shuffle_up_2(temp)     # torch.Size([Batch, 16, 128, 128])
-            branch_2 = self.branch2(branch_2)   # torch.Size([Batch, 25, 128, 128])
+            branch_2 = self.shuffle_up_2(temp)
+            branch_2 = self.branch2(branch_2)
             # 1x
-            branch_3 = self.branch3(temp)          # torch.Size([Batch, 25, 64, 64])
-            
+            branch_3 = self.branch3(temp)
+
             output1 = torch.stack((self.pixel_conv(branch_1[...,0], lap_1[...,0]),
                                    self.pixel_conv(branch_1[...,1], lap_1[...,1])), dim=-1)
             output2 = torch.stack((self.pixel_conv(branch_2[...,0], lap_2[...,0]),
@@ -67,13 +88,8 @@ class Net(nn.Module):
 
             output = self.pyramid_reconstruct(output3, output2)
             output = self.pyramid_reconstruct(output, output1)
-            #xd = fastmri.complex_abs(output)
-            #fig = plt.figure()
-            #plt.imshow(xd[0][0].detach().numpy(), cmap='gray')
-            temp = fft_utils.ifft2((1.0 - mask) * fft_utils.fft2(output) + x_k) #DataConsistency Layer
-        
-        temp = T.complex_center_crop(temp, (320,320))
-
+            # The DataConsistency Layer step is done here
+            temp = fft_utils.ifft2((1.0 - mask) * fft_utils.fft2(output) + x_k)
         return temp
 
 class PerPixelConv(nn.Module):
@@ -84,6 +100,9 @@ class PerPixelConv(nn.Module):
         b, ksize2, h, w = kernel.size()
         ksize = np.int(np.sqrt(ksize2))
         padding = (ksize - 1) // 2
+        # Before computing the per pixel convolution step we need to prepare the
+        # inputs by doing some operation like padding and reshape in order to compute
+        # the multiplication between matrices
         image = F.pad(image, (padding, padding, padding, padding))
         image = image.unfold(2, ksize, 1).unfold(3, ksize, 1)
         image = image.permute(0, 2, 3, 1, 5, 4).contiguous()
@@ -94,7 +113,7 @@ class PerPixelConv(nn.Module):
         output = output.permute(0, 3, 1, 2).contiguous()
         return output
 
-
+# The residual-block is used in the ConvBlock1 loop
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(ResidualBlock, self).__init__()
@@ -107,27 +126,31 @@ class ResidualBlock(nn.Module):
         return x + self.residualblock(x)
 
 
-class Backbone(nn.Module):
+class ConvBlock1(nn.Module):
     def __init__(self, in_channels, out_channels=64):
-        super(Backbone, self).__init__()
-        backbone = [ComplexConv2d(in_channels, out_channels, kernel_size=3, padding=1), nn.ReLU()]
+        super(ConvBlock1, self).__init__()
+        convBlock1 = [ComplexConv2d(in_channels, out_channels, kernel_size=3, padding=1), nn.ReLU()]
+        # Due to performance issues, we needed to reduce the conv block cycle steps from 16 to 2
         for i in range(4):
-            backbone.append(ResidualBlock(out_channels,out_channels))
-        backbone.append(ComplexConv2d(out_channels, out_channels, kernel_size=3, padding=1))
-        backbone.append(nn.ReLU())
-        self.backbone = nn.Sequential(*backbone)
+            convBlock1.append(ResidualBlock(out_channels,out_channels))
+        convBlock1.append(ComplexConv2d(out_channels, out_channels, kernel_size=3, padding=1))
+        convBlock1.append(nn.ReLU())
+        self.convBlock1 = nn.Sequential(*convBlock1)
 
     def forward(self, x):
-        return self.backbone(x)
+        return self.convBlock1(x)
 
 
 class PyramidDecompose(nn.Module):
+    # We take the input image and we decompose it in order to have the 2 gaussian images and a
+    # laplacian one. This will be useful later when performing the per-pixel convolution step
     def __init__(self):
         super(PyramidDecompose, self).__init__()
         kernel = np.float32([1, 4, 6, 4, 1])
+        # Outer product between kernel
         kernel = np.outer(kernel, kernel)
         kernel = kernel[:, :, None, None] / kernel.sum()
-        kernel = torch.from_numpy(np.transpose(kernel, (2, 3, 0, 1)))                # (1, 1, 256, 256, 2)
+        kernel = torch.from_numpy(np.transpose(kernel, (2, 3, 0, 1)))
         self.downfilter = nn.Conv2d(1, 1, kernel_size=5, stride=1, padding=2, bias=False)
         self.downfilter.weight = nn.Parameter(kernel, requires_grad=False)
         self.upfilter = nn.Conv2d(1, 1, kernel_size=5, stride=1, padding=2, bias=False)
@@ -141,21 +164,24 @@ class PyramidDecompose(nn.Module):
     def upsample(x):
         b, c, hin, win = x.size()
         hout, wout = hin * 2, win * 2
-        x_upsample = torch.zeros((b, c, hout, wout))#, device='cuda')
+        x_upsample = torch.zeros((b, c, hout, wout), device='cuda')
         x_upsample[:, :, ::2, ::2] = x
         return x_upsample
 
     def forward(self, x):
+        # the input image of the gaussian matrix is already given, since we use the input image
         gaussian_pyramid = [x]
         laplacian_pyramid = []
+        # we want 2 images, so the reduction step will be done two times
         for i in range(2):
             downsample_result = torch.stack((self.downsample(self.downfilter(gaussian_pyramid[-1][..., 0])),
-                                             self.downsample(self.downfilter(gaussian_pyramid[-1][..., 1]))), dim=-1)   # g = [256]         g = [256, 128]
-            gaussian_pyramid.append(downsample_result)                                  # g = [256, 128]    g = [256, 128, 64]
+                                             self.downsample(self.downfilter(gaussian_pyramid[-1][..., 1]))), dim=-1)
+            gaussian_pyramid.append(downsample_result)
             upsample_result = torch.stack((self.upfilter(self.upsample(downsample_result[..., 0])),
                                            self.upfilter(self.upsample(downsample_result[..., 1]))), dim=-1)
+            # residual is the first residual of the 'laplacian' image
             residual = gaussian_pyramid[-2] - upsample_result
-            laplacian_pyramid.append(residual)                                          # l = [256]         l = [256, 128]
+            laplacian_pyramid.append(residual)
         return gaussian_pyramid, laplacian_pyramid
 
 
@@ -173,7 +199,7 @@ class PyramidReconstruct(nn.Module):
     def upsample(x):
         b, c, hin, win = x.size()
         hout, wout = hin * 2, win * 2
-        x_upsample = torch.zeros((b, c, hout, wout))#, device='cuda')
+        x_upsample = torch.zeros((b, c, hout, wout), device='cuda')
         x_upsample[:, :, ::2, ::2] = x
         return x_upsample
 
@@ -184,6 +210,7 @@ class PyramidReconstruct(nn.Module):
 
 
 class ComplexShuffleDown(nn.Module):
+    # Perform resize
     def __init__(self, scale):
         super(ComplexShuffleDown, self).__init__()
         self.scale = scale
@@ -200,6 +227,7 @@ class ComplexShuffleDown(nn.Module):
 
 
 class ComplexShuffleUp(nn.Module):
+    # Perform resize
     def __init__(self, scale):
         super(ComplexShuffleUp, self).__init__()
         self.scale = scale
@@ -214,19 +242,8 @@ class ComplexShuffleUp(nn.Module):
         output = output.view(b, cout, hout, wout, complex_c)
         return output
 
-
-class DataConsistency(nn.Module):
-    def __init__(self):
-        super(DataConsistency, self).__init__()
-
-    def forward(self, x, x_k, mask):
-        output = torch.rand(size=x.size()).cuda()
-        for i in range(len(x)):
-            output[i] = fft_utils.ifft2(x_k[i] + fft_utils.fft2(x[i]) * (1.0 - mask[i]))
-        return output
-
-
 class ComplexConv2d(nn.Module):
+    # In order to compute the convolution step on complex data that required an additive dimension
     def __init__(self, in_channel, out_channel, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
         super(ComplexConv2d, self).__init__()
 
@@ -235,9 +252,8 @@ class ComplexConv2d(nn.Module):
         self.conv_imag = nn.Conv2d(in_channel, out_channel, kernel_size, stride=stride, padding=padding,
                                  dilation=dilation, groups=groups, bias=bias)
 
-    def forward(self, x):  # x.shape = (batch, channel, axis1, axis2, 2)
+    def forward(self, x):
         real = self.conv_real(x[..., 0]) - self.conv_imag(x[..., 1])
         imag = self.conv_imag(x[..., 0]) + self.conv_real(x[..., 1])
         output = torch.stack((real, imag), dim=4)
-        #print("SCUSI SA L'ORA????, ",output.shape)
         return output
